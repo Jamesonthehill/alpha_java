93f12e450f9b6dfc75365761784a7be050395914
건우 <dlrjsdn53@naver.com>
2022-03-08 01:04:14 +0900

    Car, CarExample

---
+++ src/com/doormain/ch01/Car.java
@@ -0,0 +1,13 @@
+package com.doormain.ch01;
+
+public class Car {
+		String name;
+		String company;
+		int price = 25000;
+		
+	public void CarExample() {
+		System.out.println("자동차 이름: " + name);
+		System.out.println("자동차 브랜드: " + company);
+		System.out.println("자동차 가격: " + price);
+	}
+}
---
+++ src/com/doormain/ch01/CarExample.java
@@ -0,0 +1,17 @@
+package com.doormain.ch01;
+
+public class CarExample extends Car{
+	private	CarExample(String name, String company, int price){
+			this.name = name;
+			this.company = company;
+			this.price = price;
+					
+		}
+		
+	public static void main(String[] args) {
+			CarExample carExample = new CarExample("R8" , "º¥Âê", 3000000);
+				carExample.CarExample();
+				
+	}
+
+}
