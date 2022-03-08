1e5cee807d4cdb10fe705b6c0187618bf73f650a
건우 <dlrjsdn53@naver.com>
2022-03-09 01:29:57 +0900

    ob, ObExample 오버로딩은 매개변수 생성시 부모클래스로 해줘야된다.

---
+++ src/com/doormain/ch02/Ob.java
@@ -0,0 +1,16 @@
+package com.doormain.ch02;
+
+public class Ob {
+		void Hot() {	
+				System.out.println("출발합니다");
+			}
+		void Hot(int a, int b) { 
+				System.out.println( a * b);
+			}
+		void Hot(int c) {
+				System.out.println("속도를 줄입니다." + c);
+		}
+		void Hot(String d) {
+				System.out.println("장비를 정지합니다.." + d);
+	}
+}
---
+++ src/com/doormain/ch02/ObExample.java
@@ -0,0 +1,13 @@
+package com.doormain.ch02;
+
+public class ObExample {
+
+		public static void main(String[] args) {
+		Ob obexample = new Ob();
+		obexample.Hot();
+		obexample.Hot(2 ,  10);
+		obexample.Hot(300);
+		obexample.Hot("오버라이드");
+		}
+
+}
